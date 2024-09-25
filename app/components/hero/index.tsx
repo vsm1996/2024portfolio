5531b4f0f4ff33905ed4b0cd7f8de78a070e3b09
Vanessa Martin <vanessa.s.martin96@gmail.com>
2024-09-25 15:47:55 -0700

    fix button hero breakpoint

--- app/components/hero/index.tsx
+++ app/components/hero/index.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
   return (
     <section ref={sectionRef} id='about' className='relative w-full bg-gradient-to-b from-primary-content to-accent-content text-white pb-16 md:pb-12' style={{ clipPath: 'polygon(0 0, 100% 0%, 100% 79%, 0 100%)' }}>
       <Navbar />
-      <div className={`opacity-0 px-12 mt-12 pb-16 md:px-24 md:pt-24 md:pb-36 flex flex-col items-center justify-between md:flex-row  ${isVisible ? `motion-safe:animate-fadeInRight` : ''}`}>
+      <div className={`opacity-0 px-12 mt-12 pb-16 md:px-24 md:pt-24 md:pb-36 flex flex-col items-center justify-between lg:flex-row  ${isVisible ? `motion-safe:animate-fadeInRight` : ''}`}>
         <div className='group mt-6 md:m-0'>
           <h1 className='text-7xl md:w-1/4 md:text-9xl font-bold text-balance'>
             Vanessa
