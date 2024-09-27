59361577d443ccdbe0714415ab592d6e302c19de
Vanessa Martin <vanessa.s.martin96@gmail.com>
2024-09-27 11:41:45 -0700

    adjustment to icon size

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -8,20 +8,20 @@ const Footer = () => {
   return (
     <div className='w-5/12 md:w-2/12 flex items-center justify-around mt-7'>
       <Link
-        className='group relative p-7 hover:motion-safe:animate-bounce ease-in-out duration-300 hover:transition-all'
+        className='group relative p-6 hover:motion-safe:animate-bounce ease-in-out duration-300 hover:transition-all'
         target='_blank'
         href={'https://www.linkedin.com/in/vsm1996'}
         aria-label="Link to Vanessa's LinkedIn Profile"
       >
-        <Image width={70} src={LinkedInLogo} alt='linkedin logo' className='group-hover:scale-125 group-active:scale-100' />
+        <Image width={63} src={LinkedInLogo} alt='linkedin logo' className='group-hover:scale-125 group-active:scale-100' />
       </Link>
       <Link
-        className='group relative p-7 hover:motion-safe:animate-bounce ease-in-out duration-300 hover:transition-all'
+        className='group relative p-6 hover:motion-safe:animate-bounce ease-in-out duration-300 hover:transition-all'
         target='_blank'
         href={'https://github.com/vsm1996/'}
         aria-label="Link to Vanessa's GitHub Profile"
       >
-        <Image width={70} src={GithubLogo} alt='github logo' className='group-hover:scale-125 group-active:scale-100' />
+        <Image width={63} src={GithubLogo} alt='github logo' className='group-hover:scale-125 group-active:scale-100' />
       </Link>
     </div>
   )
